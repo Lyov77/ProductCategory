fca82dba9715efbd19b5c57b7eb9418dad901ad6
Lyov77 <levonzargaryan@gmail.com>
2024-03-25 20:48:37 +0400

    Add project files.

---
+++ ProductCategory.Data/ProductCategoryGenerator.cs
@@ -0,0 +1,32 @@
+﻿namespace ProductCategory.Data
+{
+    public static class ProductCategoryGenerator
+    {
+        public static void GenerateProductCategories(List<Product> products)
+        {
+            // Продукты с категориями
+            var productsWithCategories = products
+                .SelectMany(product => product.Categories.Select(category => new { ProductName = product.Name, CategoryName = category }))
+                .ToList();
+
+            // Продукты, у которых нет категорий
+            var productsWithoutCategories = products
+                .Where(product => !product.Categories.Any())
+                .Select(product => product.Name)
+                .ToList();
+
+            // Вывод результатов
+            Console.WriteLine("Пары \"Имя продукта - Имя категории\":");
+            foreach (var pair in productsWithCategories)
+            {
+                Console.WriteLine($"{pair.ProductName} - {pair.CategoryName}");
+            }
+
+            Console.WriteLine("\nПродукты без категорий:");
+            foreach (var productName in productsWithoutCategories)
+            {
+                Console.WriteLine(productName);
+            }
+        }
+    }
+}
---
+++ ProductCategory/Program.cs
@@ -0,0 +1,23 @@
+﻿using ProductCategory.Data;
+
+namespace ProductCategory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = new List<Product>
+            {
+                new Product { Name = "Macbook", Categories = new List<string> { "Tech", "Laptop" } },
+                new Product { Name = "Iphone 15", Categories = new List<string> { "Tech", "Phones" } },
+                new Product{ Name = "Table", Categories = new List<string>()},
+                new Product { Name = "Samsung S14", Categories = new List<string> { "Phones" } },
+                new Product { Name = "Book", Categories = new List<string>() },
+                new Product { Name = "XBox", Categories = new List<string> { "Console", "Gaming" } },
+            };
+
+            // Обработка продуктов и категорий
+            ProductCategoryGenerator.GenerateProductCategories(products);
+        }
+    }
+}
